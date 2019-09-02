e8e3fc4b3584921cf34457b6c029038d9aec4db5
Tillhouse26 <liam.tille@gmail.com>
2019-09-02 12:58:34 +1000

    Update user.pp

--- site/profile/manifests/user.pp
+++ site/profile/manifests/user.pp
@@ -2,7 +2,6 @@ class profile::user {
   user {'testuser':
     ensure             => 'present',
     comment            => 'Test User',
-    gid                => 1100,
     groups             => ['wheel','testgrp'],
     shell              => '/bin/bash',
     uid                => 1105,
