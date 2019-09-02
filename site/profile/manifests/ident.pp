35d57d6044e24253b757f1e9426ff4177be6ab5b
Tillhouse26 <liam.tille@gmail.com>
2019-09-02 15:30:35 +1000

    Update ident.pp

--- site/profile/manifests/ident.pp
+++ site/profile/manifests/ident.pp
@@ -10,18 +10,6 @@ class profile::ident {
          #Map the values from Hiera
          $resource_title: * => $params ;
       }
-    }
-    
-    #Admin Users
-    lookup('users::admin', Hash, 'first', {}).each | $resource_title, $params| { 
-      user {
-        default:
-          ensure     => 'present',
-          home       => "/home/${params['name']}",
-          
-         #Map the values from Hiera
-         $resource_title: * => $params ;
-      }
-    }
+    }    
 
 }
