16edac932d065a42371078e36781d6f3541aa69a
Tillhouse26 <liam.tille@gmail.com>
2019-09-03 13:20:30 +1000

    Update ident.pp

--- site/profile/manifests/ident.pp
+++ site/profile/manifests/ident.pp
@@ -2,19 +2,25 @@ class profile::ident {
     
     #Active Users
     lookup('users::active', Hash, 'first', {}).each | $resource_title, $params| { 
-      user {
-        default:
-          ensure             => 'present',
-          managehome         => 'true',
-          home               => "/home/${params['name']}",
-          password_max_age   => 900,
-          password_min_age   => 7,
-          password_warn_days => 7,
-          shell              => '/bin/bash',;
-          
-         #Map the values from Hiera
-         $resource_title: * => $params ;
-      }
+    
+        group { $params['name']: 
+            ensure => 'present',
+            gid    => params['gid'],
+        }
+    
+        user {
+            default:
+              ensure             => 'present',
+              managehome         => 'true',
+              home               => "/home/${params['name']}",
+              password_max_age   => 900,
+              password_min_age   => 7,
+              password_warn_days => 7,
+              shell              => '/bin/bash',;
+
+             #Map the values from Hiera
+             $resource_title: * => $params ;
+         }
     }   
     
     #inactive Users
